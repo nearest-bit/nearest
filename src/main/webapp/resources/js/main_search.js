95595a52fae6c40823b3a2e0bef4e99f45d5fc09
Sang-Hyuk <ysh9579@naver.com>
2016-08-11 13:20:18 +0900

    페이징 처리 '이전 버튼 처리 전'

--- src/main/webapp/resources/js/main_search.js
+++ src/main/webapp/resources/js/main_search.js
@@ -3,10 +3,17 @@ var prodListTemplete = Handlebars.compile(prodListSource);
 var pageNavSource = $('#nearest-product-pagenav-template').text();
 var pageNavTemplete = Handlebars.compile(pageNavSource);
 
-/* page 버튼 누르면 재검색 하기위한 변수 */
+/* 검색 변수 */
 var searchTag = "";
 var searchContent = "";
+var pageUnit = "" ;
+var totalPage = "";
+var currentPage = "";
+var nextPage = "";
+var priviousPage = "";
+var total = "";
 
+// 검색 했을때 
 $('#nearest-search').click(function(){
 
   if($('input[name=searchContent]').val().trim() == ""){
@@ -36,16 +43,21 @@ $('#nearest-search').click(function(){
         
       alert(JSON.stringify(result.total));
         
-      var pageUnit;
+      total = JSON.stringify(result.total);
         
-      if ( JSON.stringify(result.total) % 9 != 0){
-      	pageUnit = parseInt( JSON.stringify(result.total) / 9 ) + 1;
+      if ( total % 9 != 0){
+      	totalPage = parseInt( total / 9 ) + 1;
       }else{
-      	pageUnit = parseInt( JSON.stringify(result.total) / 9 );
+    	totalPage = parseInt( total / 9 );
       }
-        
-      if ( pageUnit >= 5) {
+      
+      if ( totalPage >= 5) {
     	  pageUnit = 5; 
+    	  nextPage = pageUnit + 1;
+    	  
+    	  $('span[data-next-page=""]').attr('data-next-page', nextPage);
+      }else if( totalPage == 0 ){
+    	  pageUnit = 0;
       }
         
       if (pageUnit >= 1){
@@ -55,7 +67,7 @@ $('#nearest-search').click(function(){
       }
       searchTag = $('select[name=searchTag]').val();
       searchContent = $('input[name=searchContent]').val();
-      alert(pageUnit+'//'+searchTag+'//'+searchContent);
+      alert(pageUnit+'//'+searchTag+'//'+searchContent+'//'+totalPage);
     },
     error : function() {
       alert("error....");
@@ -63,9 +75,14 @@ $('#nearest-search').click(function(){
   });
 });
 
+//페이지 번호 눌렀을때
 $(document).on('click','#nearest-pageno > li',function (){
-	alert(searchTag+'//'+searchContent+$(this).children('a').text());
+	
 	$('#nearest-product-list').children().remove();
+	
+	currentpage = $(this).children('a').text();
+	
+	alert(searchTag+'//'+searchContent+"//"+currentpage);
 	
 	$.ajax({
 	    url :  contextRoot + 'product/list.do',
@@ -75,7 +92,7 @@ $(document).on('click','#nearest-pageno > li',function (){
 	    data : {
 	      searchTag : searchTag,
 	      searchContent : searchContent,
-	      currentPage : $(this).children('a').text()
+	      currentPage : currentpage
 	    },
 	    success : function(result) {
 	    	if(result.status != 'success'){
@@ -86,6 +103,64 @@ $(document).on('click','#nearest-pageno > li',function (){
 	    },
 	    error : function() {
 	        alert("error....");
-	      }
+	    }
+  });
+});
+
+//다음 버튼 눌렀을때
+$(document).on('click', 'a[aria-label="Next"]', function() {
+	
+	alert('currentPage = ' + $(this).children('span').attr('data-next-page'));
+	
+	currentPage = $(this).children('span').attr('data-next-page');
+	
+	$('#nearest-product-list').children().remove();
+	  $('#nearest-pageno').children().remove();
+	  
+	  $.ajax({
+	    url :  contextRoot + 'product/list.do',
+		datatype : 'json',
+		method : 'post',
+	  
+		data : {
+		  searchTag : searchTag,
+		  searchContent : searchContent,
+		  currentPage : currentPage
+		},
+		success : function(result) {
+		  if(result.status != 'success'){
+		    alert('검색오류');
+		    return;
+		  }
+		    
+		  $('#nearest-product-list').append(prodListTemplete(result));
+		    
+		  alert(JSON.stringify(result.total));
+		    
+		  if( totalPage >= (nextPage + 5)) {
+			  pageUnit = (nextPage + 5);
+			  
+			  $('span[data-next-page=""]').attr('data-next-page', nextPage);
+		  }else{
+			  pageUnit = totalPage+1;
+		  }
+		 
+		  alert(totalPage+'//'+nextPage+"//"+pageUnit);
+		 
+		  if (pageUnit >= 1){
+		  	for(var i=nextPage; i < pageUnit; i++){
+		   		$('#nearest-pageno').append(pageNavTemplete({i}));   		
+		   	}
+		  	
+		  }
+		  if(totalPage >= pageUnit){
+		      nextPage = pageUnit;
+			  $('span[data-next-page]').attr('data-next-page', nextPage);
+		  }
+		  alert(pageUnit+'//'+searchTag+'//'+searchContent);
+		},
+		error : function() {
+		  alert("error....");
+		}
   });
 });
