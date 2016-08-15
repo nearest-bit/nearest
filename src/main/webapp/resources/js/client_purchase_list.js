52b3fde96f3facb9510c4f0e5c25c925db5a718a
Sang-Hyuk <ysh9579@naver.com>
2016-08-15 21:52:35 +0900

    구매내역 UI 만들기 (codepen 에서 bootstrap date picker 와 jquery date timepicker 사용한거 이쁜거 따옴) // npm 추가 bootstrap date picker, jquery-date-picker

--- src/main/webapp/resources/js/client_purchase_list.js
+++ src/main/webapp/resources/js/client_purchase_list.js
@@ -2,7 +2,7 @@
  * 
  */
 
-$(function() {
+/*$(function() {
 	$('#order-btn').on('click', function() {
 		$('#nearest-order-search').css('display', 'block');
 		$('#order-btn').css('display', 'none');
@@ -28,4 +28,25 @@ $(function() {
 		$('#request-search').css('display', 'none');
 		$('#request-btn').css('display', '');
 	});
+});*/
+
+// 주문 기간 설정
+
+var dateSelect     = $('#flight-datepicker');
+var dateDepart     = $('#start-date');
+var dateReturn     = $('#end-date');
+var spanDepart     = $('.date-depart');
+var spanReturn     = $('.date-return');
+var spanDateFormat = 'ddd, MMMM D yyyy';
+
+dateSelect.datepicker({
+  autoclose: true,
+  format: "yy/mm/dd",
+  maxViewMode: 0,
+  startDate: "now"
+}).on('change', function() {
+  var start = $.format.date(dateDepart.datepicker('getDate'), spanDateFormat);
+  var end = $.format.date(dateReturn.datepicker('getDate'), spanDateFormat);
+  spanDepart.text(start);
+  spanReturn.text(end);
 });
