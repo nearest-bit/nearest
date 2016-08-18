463a0119da4b999ee9f576037c5dba02fedd977c
Sang-Hyuk <ysh9579@naver.com>
2016-08-18 18:31:32 +0900

    결제 완료 후 주문 상품주문 insert 구현중

--- src/main/java/org/nearest/controller/OrderController.java
+++ src/main/java/org/nearest/controller/OrderController.java
@@ -9,6 +9,7 @@ import java.util.Set;
 
 import javax.servlet.http.HttpSession;
 
+import org.nearest.domain.Client;
 import org.nearest.service.OrderService;
 import org.nearest.service.ProductOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,31 +27,35 @@ public class OrderController {
   
   @RequestMapping(path = "addOrder", produces = "applicetion/json;charset=utf-8")
   @ResponseBody
-  public String addOrder(HttpSession session, List<Integer> martNo, List<Integer> prodNo, List<Integer> prodEnt){
-//    int clientNo = ((Client)session.getAttribute("userId")).getNo();
+  public String addOrder(HttpSession session, ArrayList<Integer> martNo, ArrayList<Integer> prodNo, ArrayList<Integer> prodEnt){
+    int clientNo = ((Client)session.getAttribute("loginId")).getNo();
     
     //마트 중복 제거
     Set<Integer> removeDuplication = new HashSet<>(martNo);
     List<Integer> noDuplMartNo = new ArrayList<>(removeDuplication);
     
     Map<String, Object> params = new HashMap<>();
-    List<Integer> orderDatas = new ArrayList<>();
     
-    // 주문 테이블 추가
-    params.put("martNo", noDuplMartNo);
-    params.put("clientNo", "1");
+    List<List<Integer>> orderDatas = new ArrayList<>();
+    orderDatas.add(martNo);
+    orderDatas.add(prodNo);
+    orderDatas.add(prodEnt);
+    orderDatas.add(noDuplMartNo);
+    
+    params.put("allOrderData", orderDatas);
+    params.put("clientNo", clientNo);
     
     orderService.addOrder(params);
     
     System.out.println(params.get("orderNo"));
-    //상품 주문 테이블 추가
+    /*//상품 주문 테이블 추가
     params.put("orderNo", params.get("orderNo"));//리스트가 아닌데 포문을돌것인가??
     params.put("prodNo", prodNo);
     params.put("entity", prodEnt);
     
     
     System.out.println(params);
-    productOrderService.addProdOrder(params);
+    productOrderService.addProdOrder(params);*/
     
     
     return null;
