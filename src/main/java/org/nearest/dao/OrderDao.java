fee93ffd588f25ed346fbbea247139138539d8f7
Sang-Hyuk <ysh9579@naver.com>
2016-08-28 15:59:07 +0900

    구매목록 구현 완료 (조회 아직 안함)

--- src/main/java/org/nearest/dao/OrderDao.java
+++ src/main/java/org/nearest/dao/OrderDao.java
@@ -3,9 +3,13 @@ package org.nearest.dao;
 import java.util.List;
 import java.util.Map;
 
+import org.nearest.domain.Order;
+
 public interface OrderDao {
     int insertOrder(Map<String, Object> params);
     Map<String, Object> selectOrderCount(int clientNo);
     List<Object> selectOrderList(int clientNo);
+    Order selectOrderInfoOne(Map<String, Integer> params);
+    List<Map<String, Object>> selectOrderDetailList(Map<String, Integer> params);
     
 }
